51d8afd832a2f0183264e6df14e7b4379e231824
Shpak Andrew <shpakandrew15@gmail.com>
2026-07-01 09:41:22 +0300

    Rename tamagoci_main.py to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+
+TOKEN = "8912217606:AAFAxQKalVqR1RoDNB0zNP41LfZHJn0XXzU"
+
+bot = telebot.TeleBot(TOKEN)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я бот.")
+
+# Запуск бота
+if __name__ == "__main__":
+    print("Бот запущен...")
+    bot.infinity_polling()
+
+def test
